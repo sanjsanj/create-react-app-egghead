f91bc10c41db462d98fd84752d347c152673a95b
Sanjay Purswani <sanjsanj@hotmail.com>
2017-04-08 18:07:53 +0100

    Props validation

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import React from 'react';
 class App extends React.Component {
     render() {
         return(
-            <div>
-                <Button>I <Heart /> React</Button>
-            </div>
+            <Title text="the Text"/>
         )
     }
 }
 
-const Button = (props) => <button>{props.children}</button>
+const Title = (props) => <h1>Title: {props.text}</h1>
 
-class Heart extends React.Component {
-    render() {
-        return (
-            <span>&hearts;</span>
-        )
+Title.propTypes = {
+    text(props, propName, component) {
+        if(!(propName in props)) {
+            return new Error(`Missing ${propName}`)
+        }
+        if(props[propName].length < 6) {
+            return new Error(`${propName} was too short`)
+        }
     }
 }
 

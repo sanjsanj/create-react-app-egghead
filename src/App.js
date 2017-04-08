7cef87a38ffd81120cf3cec5c2c924cef25687fe
Sanjay Purswani <sanjsanj@hotmail.com>
2017-04-08 19:06:21 +0100

    Get refs for specific components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 
 class App extends React.Component {
     constructor() {
@@ -7,26 +8,40 @@ class App extends React.Component {
         this.update = this.update.bind(this)
     }
 
-    update(event) {
-        this.setState({currentEvent: event.type})
+    update() {
+        this.setState({
+            a: ReactDOM.findDOMNode(this.a).value,
+            b: this.refs.b.value
+        })
     }
 
     render() {
         return(
             <div>
-                <textarea
-                    onKeyPress={this.update}
-                    onCopy={this.update}
-                    onCut={this.update}
-                    onPaste={this.update}
-                    onFocus={this.update}
-                    onBlur={this.update}
-                    onDoubleClick={this.update}
-                    cols="30"
-                    rows="10" />
-                
-                <h1>{this.state.currentEvent}</h1>
+                <input
+                ref={ component => this.a = component }
+                type="text"
+                onChange={this.update.bind(this)}
+                />
+                {this.state.a}
+
+                <hr />
+
+                <input
+                ref="b"
+                type="text"
+                onChange={this.update.bind(this)}
+                />
+                {this.state.b}
             </div>
+        )
+    }
+}
+
+class Input extends React.Component {
+    render() {
+        return(
+            <input type="text" onChange={this.props.update} />
         )
     }
 }

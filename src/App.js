cd7b6a71e33193ff6f4edf1c524f369635b09142
Sanjay Purswani <sanjsanj@hotmail.com>
2017-04-08 18:13:58 +0100

    Synthetic events

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {currentEvent: "---"}
+        this.update = this.update.bind(this)
+    }
+
+    update(event) {
+        this.setState({currentEvent: event.type})
+    }
+
     render() {
         return(
-            <Title text="the Text"/>
+            <div>
+                <textarea
+                    onKeyPress={this.update}
+                    onCopy={this.update}
+                    onCut={this.update}
+                    onPaste={this.update}
+                    onFocus={this.update}
+                    onBlur={this.update}
+                    onDoubleClick={this.update}
+                    cols="30"
+                    rows="10" />
+                
+                <h1>{this.state.currentEvent}</h1>
+            </div>
         )
-    }
-}
-
-const Title = (props) => <h1>Title: {props.text}</h1>
-
-Title.propTypes = {
-    text(props, propName, component) {
-        if(!(propName in props)) {
-            return new Error(`Missing ${propName}`)
-        }
-        if(props[propName].length < 6) {
-            return new Error(`${propName} was too short`)
-        }
     }
 }
 
